2f9293b083797b03e9e39e73384042c4e50f36cf
Nkundibiza Ally <nkundibizaally@gmail.com>
2020-05-01 18:50:48 +0200

    Added a constructor, get and setters to our Person class

--- app/src/main/java/com/cyogere/simplelist/Person.java
+++ app/src/main/java/com/cyogere/simplelist/Person.java
@@ -1,4 +1,38 @@
 package com.cyogere.simplelist;
 
 public class Person {
+
+    private String name;
+    private String birthday;
+    private String gender;
+
+    public Person(String name, String birthday, String gender) {
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
