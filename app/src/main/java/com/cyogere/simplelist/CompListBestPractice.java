3c5dc799805bab521742f1f038911df0fd560987
Nkundibiza Ally <nkundibizaally@gmail.com>
2020-05-02 12:36:55 +0200

    Created ComplexList best practice activity

---
+++ app/src/main/java/com/cyogere/simplelist/CompListBestPractice.java
@@ -0,0 +1,14 @@
+package com.cyogere.simplelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompListBestPractice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comp_list_best_practice);
+    }
+}
