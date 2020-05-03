be25c9648ab177b5946a5d8b4bff2e47a4660778
Nkundibiza Ally <nkundibizaally@gmail.com>
2020-05-03 16:06:03 +0200

    Implemented CompListBestPracticeWithPicture and created its adapter

--- app/src/main/java/com/cyogere/simplelist/CompListBestPracticeWithPictures.java
+++ app/src/main/java/com/cyogere/simplelist/CompListBestPracticeWithPictures.java
@@ -3,6 +3,9 @@ package com.cyogere.simplelist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class CompListBestPracticeWithPictures extends AppCompatActivity {
 
@@ -10,5 +13,49 @@ public class CompListBestPracticeWithPictures extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comp_list_best_practice_with_pictures);
+
+        Person ally = new Person("Ally", "22-08-1991", "Male");
+        Person leader = new Person("Leader", "27-12-1992", "female");
+        Person cyprian = new Person("Cyprian", "24-08-1993", "Male");
+        Person freddy = new Person("Freddy", "22-01-1987", "Male");
+        Person louise = new Person("Louise", "26-01-1978", "female");
+        Person ally1 = new Person("Ally", "22-08-1991", "Male");
+        Person leader1 = new Person("Leader", "27-12-1992", "female");
+        Person cyprian1 = new Person("Cyprian", "24-08-1993", "Male");
+        Person freddy1 = new Person("Freddy", "22-01-1987", "Male");
+        Person louise1 = new Person("Louise", "26-01-1978", "Male");
+        Person ally2 = new Person("Ally", "22-08-1991", "Male");
+        Person leader2 = new Person("Leader", "27-12-1992", "female");
+        Person cyprian2 = new Person("Cyprian", "24-08-1993", "Male");
+        Person freddy2 = new Person("Freddy", "22-01-1987", "Male");
+        Person louise2 = new Person("Louise", "26-01-1978", "female");
+        Person ally3 = new Person("Ally", "22-08-1991", "Male");
+        Person leader3 = new Person("Leader", "27-12-1992", "female");
+        Person cyprian3 = new Person("Cyprian", "24-08-1993", "Male");
+        Person freddy3 = new Person("Freddy", "22-01-1987", "Male");
+        Person louise3 = new Person("Louise", "26-01-1978", "female");
+        Person ally4 = new Person("Ally", "22-08-1991", "Male");
+        Person leader4 = new Person("Leader", "27-12-1992", "female");
+        Person cyprian4 = new Person("Cyprian", "24-08-1993", "Male");
+        Person freddy4 = new Person("Freddy", "22-01-1987", "Male");
+        Person louise4 = new Person("Louise", "26-01-1978", "female");
+        Person ally5 = new Person("Ally", "22-08-1991", "Male");
+        Person leader5 = new Person("Leader", "27-12-1992", "female");
+        Person cyprian5 = new Person("Cyprian", "24-08-1993", "Male");
+        Person freddy5 = new Person("Freddy", "22-01-1987", "Male");
+        Person louise5 = new Person("Louise", "26-01-1978", "female");
+
+        ArrayList<Person> peopleList = new ArrayList<>();
+        peopleList.add(ally); peopleList.add(leader); peopleList.add(cyprian); peopleList.add(freddy); peopleList.add(louise);
+        peopleList.add(ally1); peopleList.add(leader1); peopleList.add(cyprian1); peopleList.add(freddy1); peopleList.add(louise1);
+        peopleList.add(ally2); peopleList.add(leader2); peopleList.add(cyprian2); peopleList.add(freddy2); peopleList.add(louise2);
+        peopleList.add(ally3); peopleList.add(leader3); peopleList.add(cyprian3); peopleList.add(freddy3); peopleList.add(louise3);
+        peopleList.add(ally4); peopleList.add(leader4); peopleList.add(cyprian4); peopleList.add(freddy4); peopleList.add(louise4);
+        peopleList.add(ally5); peopleList.add(leader5); peopleList.add(cyprian5); peopleList.add(freddy5); peopleList.add(louise5);
+
+        //Create an adapter
+        PersonListAdapterBestPractice adapter = new PersonListAdapterBestPractice(this, R.layout.adapter_view_best_practice_with_pictures_layout, peopleList);
+        ListView complexListView = findViewById(R.id.pictureComplexListView);
+        complexListView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/cyogere/simplelist/PersonListAdapterBestPracticePictures.java
@@ -0,0 +1,85 @@
+package com.cyogere.simplelist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class PersonListAdapterBestPracticePictures extends ArrayAdapter<Person> {
+
+    private Context mContext;
+    private int mResource;
+    private int lastPosition = -1;
+
+    public PersonListAdapterBestPracticePictures(@NonNull Context context, int resource, @NonNull ArrayList<Person> objects) {
+        super(context, resource, objects);
+
+        this.mContext = context;
+        this.mResource = resource;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        final View resultView;
+        ViewHolder viewHolder;
+
+        String name = getItem(position).getName();
+        String birthday = getItem(position).getBirthday();
+        String gender = getItem(position).getGender();
+        Person person = new Person(name, birthday, gender);
+
+
+        if(convertView == null) {
+
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            convertView = inflater.inflate(mResource,parent, false);
+
+            viewHolder = new ViewHolder();
+            viewHolder.nameTextView = convertView.findViewById(R.id.bestPracticeNameText);
+            viewHolder.birthdayTextView = convertView.findViewById(R.id.bestPracticeBirthdayText);
+            viewHolder.genderTextView = convertView.findViewById(R.id.bestPracticeGenderText);
+
+            // ListView animation
+             resultView = convertView;
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag(); // Tag is a way to store a view in memory
+            resultView = convertView;
+        }
+
+
+        Animation animation = AnimationUtils.loadAnimation(mContext,
+                (position > lastPosition)?R.anim.loading_down : R.anim.loading_up);
+        resultView.startAnimation(animation);
+        lastPosition = position;
+
+        viewHolder.nameTextView.setText(name);
+        viewHolder.birthdayTextView.setText(birthday);
+        viewHolder.genderTextView.setText(gender);
+
+        return convertView;
+
+    }
+
+    //##############################################################################################
+       // For best practice, it's better to hold every view in a view class
+    //##############################################################################################
+    static class ViewHolder {
+
+        TextView nameTextView;
+        TextView birthdayTextView;
+        TextView genderTextView;
+    }
+}
