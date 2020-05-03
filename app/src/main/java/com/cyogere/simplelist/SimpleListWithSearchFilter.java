3e750decf0c18112057c28ea8dc4722cd035d1cc
Nkundibiza Ally <nkundibizaally@gmail.com>
2020-05-03 09:39:46 +0200

    Created a new simpleList activity with searching filter

---
+++ app/src/main/java/com/cyogere/simplelist/SimpleListWithSearchFilter.java
@@ -0,0 +1,14 @@
+package com.cyogere.simplelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimpleListWithSearchFilter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_list_with_search_filter);
+    }
+}
