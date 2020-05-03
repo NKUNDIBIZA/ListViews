91c4d8d33409272ce75b2dea0cf0cb977f41b49d
Nkundibiza Ally <nkundibizaally@gmail.com>
2020-05-03 15:46:42 +0200

    Created a complexListBestPractice activity with picture

---
+++ app/src/main/java/com/cyogere/simplelist/CompListBestPracticeWithPictures.java
@@ -0,0 +1,14 @@
+package com.cyogere.simplelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompListBestPracticeWithPictures extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comp_list_best_practice_with_pictures);
+    }
+}
